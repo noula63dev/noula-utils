bd3f99ddde43f46d31bfaddc337b5598ecc21c28
noula63dev <noula63dev.gmail.com>
2021-01-06 17:43:51 +0100

    fixed and tested usarLogin.php

--- Php/jwt-auth/userLogin.php
+++ Php/jwt-auth/userLogin.php
@@ -1,6 +1,7 @@
 <?php
 //ini_set("display_errors" , 1);
 require "./vendor/autoload.php";
+
 use \Firebase\JWT\JWT;
 
 require_once('./configs/constants.php');
@@ -11,31 +12,30 @@ header("Content-Type: application/json; charset=utf-8");
 
 
 include('./classes/mysqli2json.class.php');
-if($_SERVER['REQUEST_METHOD']  === 'POST'){
-
+if ($_SERVER['REQUEST_METHOD']  === 'POST') {
     $json = json_decode(file_get_contents('php://input'));
-    
+
     foreach ($json as $key => $value) {
         $posted[$key] = $value;
     }
-    $sql ="SELECT * FROM users WHERE Email = '$posted[email]' ";
+    $sql = "SELECT * FROM users WHERE Email = '$posted[email]' ";
 
-    $usersData = new Mysqli2Json('root', 'myl1nuxb0x','labdb', $sql);
+    $usersData = new Mysqli2Json('root', 'myl1nuxb0x', 'labdb', $sql);
     $usersData->selectData();
-    if($usersData->getRows() > 0){
+    if ($usersData->getRows() > 0) {
         $users = $usersData->getDatas();
         $id = $users['UserId'];
         $name = $users['NickName'];
         $email = $users['Email'];
         $admin = $users['Admin'];
 
-        if($users["Password"] === $posted["password"]){
+        if ($users["Password"] === $posted["password"]) {
 
             $iat = $_SERVER['REQUEST_TIME'];
 
             $exp = $_SERVER['REQUEST_TIME'] + EXPIRATION_TIME;
 
-            $user_data = array (
+            $user_data = array(
                 "userid" => $id,
                 "email" => $email,
                 "name" => $name,
@@ -54,28 +54,23 @@ if($_SERVER['REQUEST_METHOD']  === 'POST'){
 
             $jwt = JWT::encode($payload, SECRET_KEY);
 
-            $decoded = JWT::decode($jwt, SECRET_KEY , array(HASH));
+            $decoded = JWT::decode($jwt, SECRET_KEY, array(HASH));
 
             http_response_code(202);
 
-            $result = array("jwt"=> $jwt ,"user" => $user_data , "status" => 1, "msg" => "User accepted");
-        }
-        else {
-            http_response_code(401);
+            $result = array("jwt" => $jwt, "user" => $user_data, "status" => 1, "msg" => "User accepted");
+        } else {
+            //http_response_code(401);
 
-            $result = array("status" => 0, "msg" => "Unauthorized");
+            $result = array("status" => 0, "msg" => "Wrong credentials");
         }
-    }
-    else{
-        http_response_code(404);
+    } else {
+        //http_response_code(404);
 
-        $result = array("jwt"=> $jwt , "status" => 0, "msg" => "No user found");
-        
+        $result = array("jwt" => $jwt, "status" => 0, "msg" => "No user found");
     }
-}
-else{
+} else {
     http_response_code(405);
     $result = array("status" => 0, "msg" => "Method not allowed");
 }
 echo json_encode($result);
-?>
